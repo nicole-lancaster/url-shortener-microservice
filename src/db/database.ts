547646aa078863a79f17ac3415cad4774fc77dce
Nicole <lancasternj@gmail.com>
2023-09-05 09:55:15 +0100

    taking away delay on nodemon

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,27 +2,27 @@ require("dotenv").config();
 import mongoose from "mongoose";
 
 type EnvVariables = {
-    MONGO_URI: string;
-}
+  MONGO_URI: string;
+};
 
 type UrlType = {
-    short_url: number;
-    original_url: string
+  short_url: number;
+  original_url: string;
 };
 
 try {
-    mongoose.connect((process.env as EnvVariables).MONGO_URI)
-    console.log("DB connection successful!");
+  mongoose.connect((process.env as EnvVariables).MONGO_URI);
+  console.log("DB connection successful!");
 } catch (err) {
-    console.log("DB connection not successful!");
+  console.log("DB connection not successful!");
 }
-
 
 // defines the shape of the documents (rows) within that collection
 const urlSchema = new mongoose.Schema<UrlType>({
-    short_url: { type: Number, required: true },
-    original_url: { type: String, required: true }
+  short_url: { type: Number, required: true },
+  original_url: { type: String, required: true },
 });
 
 // A model allows you to create instances of your objects, called documents
-export const UrlDocument: mongoose.Model<UrlType, {}, {}> = mongoose.model<UrlType>("Url", urlSchema);
+export const UrlDocument: mongoose.Model<UrlType, {}, {}> =
+  mongoose.model<UrlType>("Url", urlSchema);
