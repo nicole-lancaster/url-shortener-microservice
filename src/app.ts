aa969ff3c5d155da79deb0321072385762427ad9
Nicole <lancasternj@gmail.com>
2023-09-05 10:09:13 +0100

    changing endpoint for basic html to /api

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ export const app = express();
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/", getBasicHtml);
+app.get("/api", getBasicHtml);
 app.post("/api/shorturl", postOriginalAndGetShort);
 app.get("/api/shorturl/:shorturl", getOriginalByInputtingShort);
 app.get("*", (_, response) =>
